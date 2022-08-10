245f7d47575c43ec6435aa0f83badd33e9e4d5b1
sahil khatiwada <sahilkhatiwada12@gmail.com>
2022-08-10 22:00:53 +0545

    added index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $p=100000;
+    $r=12;
+    $t=4;
+    $si= $p*$r*$t/100 ;
+
+     echo $si ;
+
+    ?>
+    
+</body>
+</html>
